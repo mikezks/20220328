95a01f33abe1c3ed79c78449babc9accd3edd3cb
mikezks <23002661+mikezks@users.noreply.github.com>
2022-03-28 11:53:32 +0200

    refactor: flight-service data access

--- src/app/flight-search/flight-search.component.ts
+++ src/app/flight-search/flight-search.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Flight } from '../entities/flight';
+import { FlightService } from './flight.service';
 
 @Component({
   selector: 'app-flight-search',
@@ -13,22 +14,13 @@ export class FlightSearchComponent implements OnInit {
   flights: Flight[] = [];
   selectedFlight: Flight | undefined;
 
-  constructor(private http: HttpClient) { }
+  constructor(private flightService: FlightService) { }
 
   ngOnInit(): void {
   }
 
   search(): void {
-    const url = 'http://www.angular.at/api/flight';
-
-    const params = new HttpParams()
-      .set('from', this.from)
-      .set('to', this.to);
-
-    const headers = new HttpHeaders()
-      .set('Accept', 'application/json');
-
-    this.http.get<Flight[]>(url, { params, headers })
+    this.flightService.load(this.from, this.to)
       .subscribe({
         next: flights => this.flights = flights,
         error: err => console.error('Flights loading error', err)

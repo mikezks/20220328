6065c5419409b0a12295dc7b14c14d56c7eee868
mikezks <23002661+mikezks@users.noreply.github.com>
2022-03-28 15:18:32 +0200

    feat: city pipe plane

--- src/app/shared/pipes/city.pipe.ts
+++ src/app/shared/pipes/city.pipe.ts
@@ -6,6 +6,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class CityPipe implements PipeTransform {
 
   transform(value: string): string {
-    return value + ' :)';
+    return value + ' ✈';
   }
 }

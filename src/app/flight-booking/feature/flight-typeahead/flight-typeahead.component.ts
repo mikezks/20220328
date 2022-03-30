e7d8f0f830ee6ade55446c4bd73a140066815d3a
mikezks <23002661+mikezks@users.noreply.github.com>
2022-03-30 13:23:50 +0200

    feat: reactive typeahead

--- src/app/flight-booking/feature/flight-typeahead/flight-typeahead.component.ts
+++ src/app/flight-booking/feature/flight-typeahead/flight-typeahead.component.ts
@@ -1,5 +1,8 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Observable, share, Subscription, tap, timer } from 'rxjs';
+import { FormControl } from '@angular/forms';
+import { debounceTime, distinctUntilChanged, EMPTY, filter, Observable, share, Subscription, switchMap, tap, timer } from 'rxjs';
+import { Flight } from '../../../entities/flight';
 
 @Component({
   selector: 'app-flight-typeahead',
@@ -13,10 +16,58 @@ export class FlightTypeaheadComponent implements OnInit, OnDestroy {
   );
   subscription = new Subscription();
 
-  constructor() { }
+  control = new FormControl();
+  flights$: Observable<Flight[]> = this.getFlightsStream$();
+  loading: boolean = false;
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
-    this.rxjsDemo();
+    // this.rxjsDemo();
+  }
+
+  getFlightsStream$(): Observable<Flight[]> {
+    /**
+     * Stream 1: Input value changes
+     * - Trigger
+     * - Data Provider
+     */
+    return this.control.valueChanges.pipe(
+      // Get additional state
+      // withLatestFrom(),
+      // Filtering START
+      filter(city => city.length > 2),
+      debounceTime(300),
+      distinctUntilChanged(),
+      // Filtering END
+      // Side-Effect
+      tap(() => this.loading = true),
+      /**
+       * Stream 2: Backend HTTP call -> Array of Flights
+       * - Data Provider
+       */
+      switchMap(city => this.load(city)),
+      // Transformation
+      // map(),
+      // Side-Effect
+      tap(() => this.loading = false)
+    );
+  }
+
+  /**
+   * Stream 2: Backend HTTP call -> Array of Flights
+   * - Data Provider
+   */
+  load(from: string): Observable<Flight[]>  {
+    const url = "http://www.angular.at/api/flight";
+
+    const params = new HttpParams()
+                        .set('from', from);
+
+    const headers = new HttpHeaders()
+                        .set('Accept', 'application/json');
+
+    return this.http.get<Flight[]>(url, {params, headers});
   }
 
   rxjsDemo(): void {
